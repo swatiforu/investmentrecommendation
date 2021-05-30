821b7bc1bfa2365c736a7c8b2fbf3882de6d4806
Umer Saeed <umersaeed4@gmail.com>
2021-05-30 15:08:20 +0500

    initial

---
+++ app.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import datetime
+from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/stock/', methods=['GET'])
+def stockCalculation():
+  time = requests.args['time']
+  amount = request.args['amount']
+  df = pd.read_excel('Stock'+time+'.xlsx')
+  vals = df.values[:5]
+  results = []
+  for i in range(5):
+    k = dict()
+    k['Ticker'] = vals[i][1]
+    k['Percent'] = vals[i][0]
+    k['Amount'] = amount+(amount*float(vals[i][0]))/100
+    results.append(k)
+  return results
+
+@app.route('/plot/', methods=['GET'])
+def plotCalculation():
+  time = requests.args['time']
+  amount = request.args['amount']
+  df = pd.read_excel('Plot/'time+'.xlsx')
+  vals = df.values[:5]
+  results = []
+  for i in range(5):
+    k = dict()
+    k['Ticker'] = vals[i][1]
+    k['Percent'] = vals[i][0]
+    k['Amount'] = amount+(amount*float(vals[i][0]))/100
+    results.append(k)
+  return results
