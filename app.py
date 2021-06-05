eddb7d6e534112af23a16cd520c30505407e06e4
swatiforu <46088717+swatiforu@users.noreply.github.com>
2021-06-05 09:13:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def stockCalculation():
   time = str(request.args['time'])
   amount = int(request.args['amount'])
   df = pd.read_excel('Stock/'+time+'.xlsx')
-  df['Percent'] = pd.to_numeric(df['Percent'])
+  df['Percentage'] = pd.to_numeric(df['Percent'])
   vals = df.values[:5]
   results = dict()
   for i in range(5):
